60e9b6342ca3ba5a50c42193e4139e7adb87ca0c
GSilva <93489073+G-Silvaa@users.noreply.github.com>
2023-06-02 11:57:46 -0300

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -1,3 +1,5 @@
+// const { default: axios } = require("axios");
+
 const CardPokemon = document.querySelectorAll('.js-open-details-podemon');
 const closePokemon = document.querySelector('.close')
 
@@ -16,13 +18,51 @@ CardPokemon.forEach(card => {
 
 closePokemon.addEventListener('click', closeDetailsPokemon)
 
-console.log(CardPokemon)
+
+
 
 // parte do consumo de api do pokemon
 
-axios({
-    method: 'GET',
-    url: 'https://pokeapi.co/api/v2/pokemon'
-})
 
-.then(json => console.log(json))
+function listingPokemons(urlApi) {
+    axios({
+        method: 'GET',
+        url: urlApi
+    })
+
+    .then((responde) => {
+        const numberPokemon = document.getElementById('js-number-pokemons')
+
+        const {results, next, count} = responde.data;
+
+        console.log(count)
+
+        numberPokemon.innerText = count;
+
+        results.forEach(pokemon => {
+            let urlApiDetails = pokemon.url;
+
+            axios({
+                method: 'GET',
+                url: `${urlApiDetails}`
+            })
+            .then(responde => {
+                const {name , id, sprites, types} = responde.data;
+                console.log(name)
+
+                const infocard = {
+                    name: name,
+                    code: id,
+                    Image: sprites.other.dream_world.front_default,
+                    types: types[0].type.name
+                }
+                console.log(infocard)
+            })
+        })
+    })
+}
+
+listingPokemons('https://pokeapi.co/api/v2/pokemon?limit=9&offset=0');
+
+
+
