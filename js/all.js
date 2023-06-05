ffdd3ec98f097e5a42ee7661bb6c349b2fe4846d
GSilva <93489073+G-Silvaa@users.noreply.github.com>
2023-06-05 00:57:17 -0300

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -21,8 +21,55 @@ closePokemon.addEventListener('click', closeDetailsPokemon)
 
 
 
-// parte do consumo de api do pokemon
+// parte do card pokemon
+const areaPokemon = document.getElementById('js-list-pokemon')
 
+function primeiraLetraMa(string){
+ return string.charArt(0).toUpperCase() + string.slice(1);
+}
+
+function createPoke(code, type, nome, imagePo){
+    let card = document.createElement('button');
+    card.classList = `js-open-details-podemon tamanho card-pokemon `;
+    areaPokemon.appendChild(card);
+
+   let image = document.createElement('div');
+   image.classList = `image ${type}`;
+   card.appendChild(image);
+
+   let imageSrc = document.createElement('img');
+   imageSrc.classList = 'api-tamanho'
+   imageSrc.setAttribute('src',imagePo);
+   image.appendChild(imageSrc)
+   
+    let infocard = document.createElement('div');
+    infocard.classList ='info';
+    card.appendChild(infocard)
+
+    let textcard = document.createElement('div');
+    textcard.classList = 'text'
+    infocard.appendChild(textcard)
+
+    let spancard = document.createElement('span')
+    spancard.textContent = (code < 10) ? `#00${code}`: (code < 100) ? `#0${code}` : `#${code}` ;
+    textcard.appendChild(spancard);
+
+    let  namecard = document.createElement('h3')
+    namecard.textContent = nome;
+    textcard.appendChild(namecard)
+
+    let areaicon = document.createElement('div')
+    areaicon.classList = 'icon3';
+    infocard.appendChild(areaicon)
+
+    let imgType = document.createElement('img');
+    imgType.setAttribute('src',`./assets/icon-types/${type}.svg`)
+    areaicon.appendChild(imgType)
+   
+    
+}
+
+// parte do consumo de api do pokemon
 
 function listingPokemons(urlApi) {
     axios({
@@ -35,12 +82,13 @@ function listingPokemons(urlApi) {
 
         const {results, next, count} = responde.data;
 
-        console.log(count)
-
         numberPokemon.innerText = count;
+
+        
 
         results.forEach(pokemon => {
             let urlApiDetails = pokemon.url;
+
 
             axios({
                 method: 'GET',
@@ -48,21 +96,40 @@ function listingPokemons(urlApi) {
             })
             .then(responde => {
                 const {name , id, sprites, types} = responde.data;
-                console.log(name)
+                
+               
 
                 const infocard = {
-                    name: name,
+                    nome: name,
                     code: id,
-                    Image: sprites.other.dream_world.front_default,
-                    types: types[0].type.name
+                    image: sprites.other.dream_world.front_default,
+                    type: types[0].type.name,
+                    
+                   
                 }
-                console.log(infocard)
+                
+                createPoke(infocard.code, infocard.type, infocard.nome, infocard.image);
+                
+                    
+                const cardpokemon = document.querySelectorAll('.js-open-details-podemon');
+
+                cardpokemon.forEach(card => {
+                    card.addEventListener('click', openDetailsPokemon);
+
+                    
+                })
+               
             })
         })
     })
 }
 
 listingPokemons('https://pokeapi.co/api/v2/pokemon?limit=9&offset=0');
+
+CardPokemon.forEach(card => {
+    card.addEventListener('click', openDetailsPokemon)})
+
+closePokemon.addEventListener('click', closeDetailsPokemon)
 
 
 
